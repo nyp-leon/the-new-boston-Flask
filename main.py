c812c3fae2e38c1bf60f165c791650fbb10489ac
Leon Tan <natnoel@live.com>
2018-10-16 21:52:23 +0800

    Routing

--- main.py
+++ main.py
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def index():
     return 'Home page'
 
+@app.route('/cat')
+def cat():
+    return '<h1>Meow</h1>'
+
+@app.route('/food/<food>')
+def food(food):
+    return "Go eat some %s" %food
+
+@app.route('/prisoner/<int:id>')
+def prisoner(id):
+    return "You are prisoner %s" %id
+
 if __name__ == "__main__":
     app.run(debug=True)
