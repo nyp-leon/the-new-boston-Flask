c166cac31f794ba72cb9b96bd652276ba1c91cc0
Leon <leon_tan@nyp.edu.sg>
2018-10-17 11:09:53 +0800

    HTTP Methods

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -17,6 +17,17 @@ def food(food):
 @app.route('/prisoner/<int:id>')
 def prisoner(id):
     return "You are prisoner %s" %id
+
+@app.route('/get')
+def get():
+    return "Method used: %s" % request.method
+
+@app.route("/bacon", methods=['GET', 'POST'])
+def bacon():
+    if request.method == 'POST':
+        return "u are using post"
+    else:
+        return "probably a get"
 
 if __name__ == "__main__":
     app.run(debug=True)
